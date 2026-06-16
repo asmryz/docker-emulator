ea27d368ce1ccd6dfd1d0f66a4b27a6e6c78dffa
Asim Riaz <asim.riaz@live.com>
2026-06-17 00:33:06 +0500

    API-34

--- build.sh
+++ build.sh
@@ -2,30 +2,30 @@
 
 set -x
 
-ANDROID_API_LEVEL=28
+ANDROID_API_LEVEL=34
 SCREEN_RESOLUTION=1366x768
 
-docker rmi docker-emulator
+docker rmi docker-emulator:latest || true
 
 set -ex
 
 # build openbox as base
 cd dockerfiles/ubuntu
-  docker build . -t docker-emulator
+  docker build . -t docker-emulator:ubuntu
 cd -
 
   # build openbox as base
   cd dockerfiles/openbox
-    docker build . -t docker-emulator
+    docker build . -t docker-emulator:openbox
   cd -
 
   # build appium
   cd dockerfiles/appium
-    docker build . -t docker-emulator
+    docker build . -t docker-emulator:appium
   cd -
 
   cd dockerfiles/noVNC
-    docker build . -t docker-emulator
+    docker build . -t docker-emulator:novnc
   cd -
 
   # build android emulator
@@ -33,21 +33,21 @@ cd -
     docker build -f dockerfile.base . \
       --build-arg="ANDROID_API_LEVEL=$ANDROID_API_LEVEL" \
       --build-arg="SCREEN_RESOLUTION=$SCREEN_RESOLUTION" \
-      -t docker-emulator
+      -t docker-emulator:android-base
   cd -
 
 # finialize docker
 cd dockerfiles/final
   docker build . \
     --build-arg="ANDROID_API_LEVEL=$ANDROID_API_LEVEL" \
-    -t docker-emulator
+    -t docker-emulator:final
 cd -
 
 cd dockerfiles/android
   docker build . \
     --build-arg="ANDROID_API_LEVEL=$ANDROID_API_LEVEL" \
     --build-arg="SCREEN_RESOLUTION=$SCREEN_RESOLUTION" \
-    -t docker-emulator
+    -t docker-emulator:latest
 cd -
 
 # docker image tag docker-emulator 192.168.10.61:5000/logickee/docker-emulator
--- dockerfiles/android/etc/launch_emulator.sh
+++ dockerfiles/android/etc/launch_emulator.sh
@@ -2,12 +2,20 @@
 
 set -x
 
+if [ "$ANDROID_API_LEVEL" = "34" ]; then
+  AVD_NAME="Pixel_8"
+  SYSDIR="$ANDROID_HOME/system-images/android-34/google_apis/x86_64/"
+else
+  AVD_NAME="test_nexus"
+  SYSDIR="$ANDROID_HOME/system-images/android-$ANDROID_API_LEVEL/default/x86/"
+fi
+
 /root/Android/emulator/emulator \
-  -avd test_nexus \
-  -sysdir $ANDROID_HOME/system-images/android-$ANDROID_API_LEVEL/default/x86/
-
-
-
+  -avd "$AVD_NAME" \
+  -sysdir "$SYSDIR" \
+  -no-audio \
+  -no-snapshot \
+  -no-boot-anim
 
 # cd /usr/src/app && /usr/bin/xfce4-terminal -x "npm start run"
 # cd /usr/src/app && /usr/bin/xfce4-terminal -x "npx react-native run-android"
